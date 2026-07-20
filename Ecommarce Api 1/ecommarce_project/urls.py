4cf65f4a463b9735558a3309036c9c933a6ae652
Saim Ahmed <sayeemahmed850@gmail.com>
2026-07-20 19:43:18 +0600

    url patterns in urls.py

--- Ecommarce Api 1/ecommarce_project/urls.py
+++ Ecommarce Api 1/ecommarce_project/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view # type: ignore
 from drf_yasg import openapi # type: ignore
+from django.conf import settings
+from django.conf.urls.static import static
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -24,3 +26,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/", include('api.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
